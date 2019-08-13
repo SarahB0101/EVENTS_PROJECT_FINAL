5eac407ee76eab20a64c2aa320d5ce3223c40b0d
SarahB0101 <sbouchir@gmail.com>
2019-08-13 20:46:10 +0200

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => '' }
+  config.action_mailer.default_url_options = { :host => 'pure-depths-62995.herokuapp.com' }
 end
